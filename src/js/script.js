25289c9ecaeff0e805b1c05a1982cb106980e686
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2020-12-05 03:18:45 +0200

    hw

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,16 @@ const Theme = {
   LIGHT: "light-theme",
   DARK: "dark-theme",
 };
+const refs = {
+  bodyTheme: document.querySelector('body'),
+  iconTheme: document.querySelector('.theme-switch__toggle'),
+};
+
+refs.bodyTheme.classList.add(Theme.LIGHT);
+
+refs.iconTheme.addEventListener('change', changeTheme);
+
+function changeTheme(evt) {
+  refs.bodyTheme.classList.toggle(Theme.DARK);
+  localStorage.setItem(evt.target.checked);
+}
