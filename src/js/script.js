db1489450b50b3e54bd51c263dd416d9fdceddfd
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2020-12-05 16:13:23 +0200

    var2

--- src/js/script.js
+++ src/js/script.js
@@ -11,30 +11,35 @@ const STORAGE_KEY = 'theme';
 
 refs.iconTheme.addEventListener('change', changeTheme);
 
-function setDarkTheme() {
-  refs.bodyTheme.classList.add(Theme.DARK);
-  refs.bodyTheme.classList.remove(Theme.LIGHT);
-}
-function setLightTheme() {
-  refs.bodyTheme.classList.add(Theme.LIGHT);
-  refs.bodyTheme.classList.remove(Theme.DARK);
-}
 function defaultTheme() {
   if (localStorage.classList === Theme.DARK) {
     refs.bodyTheme.classList.add(Theme.DARK);
     refs.iconTheme.cheched = true;
   }
 }
-function changeTheme() {
-  if (refs.iconTheme.checked) {
-    setDarkTheme();
-    localStorage.setItem(STORAGE_KEY, Theme.DARK);
-  } else {
-    setLightTheme();
-    localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
-  }
+defaultTheme();
+
+function changeTheme(evt) {
+  refs.bodyTheme.classList.toggle(Theme.DARK);
+  refs.bodyTheme.classList.toggle(Theme.LIGHT);
+  localStorage.setItem(evt.target.checked);
 }
-// function changeTheme(evt) {
-//   refs.bodyTheme.classList.toggle(Theme.DARK);
-//   localStorage.setItem(evt.target.checked);
+
+
+// function changeTheme() {
+//   if (refs.iconTheme.checked) {
+//     setDarkTheme();
+//     localStorage.setItem(STORAGE_KEY, Theme.DARK);
+//   } else {
+//     setLightTheme();
+//     localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
+//   }
+// }
+// function setDarkTheme() {
+//   refs.bodyTheme.classList.add(Theme.DARK);
+//   refs.bodyTheme.classList.remove(Theme.LIGHT);
+// }
+// function setLightTheme() {
+//   refs.bodyTheme.classList.add(Theme.LIGHT);
+//   refs.bodyTheme.classList.remove(Theme.DARK);
 // }
